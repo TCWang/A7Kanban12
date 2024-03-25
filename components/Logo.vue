71f7e6344da8a54f4b68ac2058f1f80fc95d8dab
TC Wang <wangtct@hotmail.com>
2024-03-25 09:04:40 +0800

    first commit

---
+++ components/Logo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div class="">
+      <NuxtLink class="font-bold text-white text-lg" to="/"
+        >A7社區網站-健康休閒</NuxtLink
+      >
+    </div>
+  </div>
+</template>
+<script></script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h1>A7新林口-社區網站: 12.健康休閒資訊平台</h1>
+    <ChartTest01 />
+  </div>
+</template>
+
+<script setup>
+useHead({
+  title: "A7-12.健康休閒",
+  meta: [
+    { name: "description", content: "A7新林口-社區網站: 12.健康休閒資訊平台 " },
+  ],
+});
+</script>
+
+<style></style>
