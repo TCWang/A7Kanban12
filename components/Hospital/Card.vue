ba5dffcfd687aee6e0808ceb128fa99d094d2542
TC Wang <wangtct@hotmail.com>
2024-05-17 05:02:29 +0800

    hospital

--- components/Hospital/Card.vue
+++ components/Hospital/Card.vue
@@ -1,21 +1,57 @@
 <template>
   <div
-    class="flex-row flex-wrap shadow border-hidden mb-1 cursor-pointer w-1/2 md:w-1/4 lg:1/6 h-[200px] md:h-[220px]"
-    @click="navigateTo(`hospital/${hospital.name}`)"
+    class="relative shadow border-hidden w-full lg:w-1/2 overflow-hidden mb-5 cursor-pointer h-full"
   >
-    <div class="flex flex-col h-full bg-zinc-100 rounded-2xl shadow-md">
+    <div
+      class="m-2 flex flex-row md:flex-row h-full bg-zinc-100 rounded-2xl shadow-md"
+      @click="navigateTo(`/hospital/${hospital.name}`)"
+    >
       <img
-        :src="`../images/hospital/${hospital.figureLogo}`"
+        :src="`/images/hospital/${hospital.figureLogo}`"
         alt=""
-        class="ml-4 w-[80px] md:w-[120px] h-[100px] md:h-[120px] rounded-xl transform hover:scale-110 duration-200"
+        class="w-[150px] md:w-[200px] h-[150px] md:h-[200px] rounded-xl transform hover:scale-110 duration-200"
       />
 
       <div class="p-4 flex flex-col">
         <div>
-          <p>{{ hospital.category }}</p>
-          <p class="bg-blue-200 hover:bolder-red px-3 py-2 rounded text-lg">
-            <a :href="hospital.web"> {{ hospital.name }} </a>
+          <p class="text-bold">{{ hospital.category }} - {{ hospital.name }}</p>
+
+          <p class="mt-auto">
+            <a :href="hospital.google"> {{ hospital.address }} </a>
           </p>
+          <p class="mt-auto">電話: {{ hospital.telphone }}</p>
+          <p v-show="hospital.email" class="mt-auto">
+            郵箱: {{ hospital.email }}
+          </p>
+          <p class="mt-auto">營業: {{ hospital.hours }}</p>
+        </div>
+
+        <div class="flex flex-row space-x-4">
+          <a v-show="hospital.web" :href="hospital.web">
+            <NuxtImg
+              class="h-12 hover:scale-150"
+              src="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-images/logo/LOG-01-Web.jpeg"
+            />
+          </a>
+          <a v-show="hospital.facebook" :href="hospital.facebook">
+            <NuxtImg
+              class="h-12 hover:scale-150"
+              src="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-images/logo/LOG-01-Facebook.png
+            "
+            />
+          </a>
+          <a v-show="hospital.line" :href="hospital.line">
+            <NuxtImg
+              class="h-12 hover:scale-150"
+              src="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-images/logo/LOG-01-Line.png"
+            />
+          </a>
+          <a v-show="hospital.ig" :href="hospital.ig">
+            <NuxtImg
+              class="h-12 hover:scale-150"
+              src="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-images/logo/LOG-01-Ig.png"
+            />
+          </a>
         </div>
       </div>
     </div>
--- components/Hospital/Detail/Hero.vue
+++ components/Hospital/Detail/Hero.vue
@@ -1,56 +1,74 @@
 <template>
-  <div class="container flex flex-col md:flex-row">
+  <div class="mt-2 flex flex-col md:flex-row">
     <img
-      :src="`../images/hospital/${hospital.figure}`"
-      class="w-[240px] h-[240px] md:w-1/2 md:h-1/2 px-1 md:px-4"
+      :src="`/images/hospital/${hospital.figure}`"
+      class="w-full md:w-1/2 md:h-1/2 md:px-4"
       alt=""
     />
 
-    <div class="md:w-1/2">
-      <h1 class="mt-1">{{ hospital.name }}</h1>
+    <div class="w-full md:w-1/2">
+      <h1 class="mt-1">{{ hospital.category }} - {{ hospital.name }}</h1>
+      <div class="text-slate-500 text-lg mt-3 border-b pb-5 justify-between">
+        <div class="flex-col">
+          <p class="">
+            地址:
+            <a :href="hospital.google">
+              {{ hospital.address }}
+            </a>
+          </p>
+          <p class="">電話: {{ hospital.telphone }}</p>
+          <p class="">營業時間: {{ hospital.hours }}</p>
+        </div>
 
-      <p>類別: {{ hospital.category }}</p>
-
-      <p>
-        網站: {{ hospital.id }} -
-        <a :href="hospital.web">
-          {{ hospital.name }}
-        </a>
-      </p>
-      <p>
-        地址:
-        <a :href="hospital.google">
-          {{ hospital.address }}
-        </a>
-      </p>
-      <p>電話: {{ hospital.telphone }}</p>
-      <p>營業時間: {{ hospital.hours }}</p>
-
-      <a v-show="hospital.web" :href="hospital.web">
-        <NuxtImg
-          class="h-12 hover:scale-150"
-          src="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-images/logo/LOG-01-Web.jpeg"
-        />
-      </a>
-      <a v-show="hospital.facebook" :href="hospital.facebook">
-        <NuxtImg
-          class="h-12 hover:scale-150"
-          src="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-images/logo/LOG-01-Facebook.png
+        <p class="flex flex-row space-x-4">
+          <a v-show="hospital.web" :href="hospital.web">
+            <NuxtImg
+              class="h-12 hover:scale-150"
+              src="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-images/logo/LOG-01-Web.jpeg"
+            />
+          </a>
+          <a v-show="hospital.facebook" :href="hospital.facebook">
+            <NuxtImg
+              class="h-12 hover:scale-150"
+              src="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-images/logo/LOG-01-Facebook.png
             "
-        />
-      </a>
-      <a v-show="hospital.line" :href="hospital.line">
-        <NuxtImg
-          class="h-12 hover:scale-150"
-          src="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-images/logo/LOG-01-Line.png"
-        />
-      </a>
-      <a v-show="hospital.ig" :href="hospital.ig">
-        <NuxtImg
-          class="h-12 hover:scale-150"
-          src="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-images/logo/LOG-01-Ig.png"
-        />
-      </a>
+            />
+          </a>
+          <a v-show="hospital.line" :href="hospital.line">
+            <NuxtImg
+              class="h-12 hover:scale-150"
+              src="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-images/logo/LOG-01-Line.png"
+            />
+          </a>
+          <a v-show="hospital.ig" :href="hospital.ig">
+            <NuxtImg
+              class="h-12 hover:scale-150"
+              src="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-images/logo/LOG-01-Ig.png"
+            />
+          </a>
+        </p>
+      </div>
+      <div class="flex flex-row space-x-4">
+        <a v-if="hospital.suggestion" :href="hospital.suggestion">
+          <button
+            class="bg-blue-500 hover:bg-blue-700 text-white hover:text-yellow-200 rounded-lg px-4 py-2"
+          >
+            推薦
+          </button>
+        </a>
+        <a v-if="hospital.suggestion2" :href="hospital.suggestion2">
+          <button
+            class="bg-blue-500 hover:bg-blue-700 text-white hover:text-yellow-200 rounded-lg px-4 py-2"
+          >
+            推薦 2
+          </button>
+        </a>
+      </div>
+      <div
+        class="mt-2"
+        v-if="hospital.description"
+        v-html="hospital.description"
+      ></div>
     </div>
   </div>
 </template>
